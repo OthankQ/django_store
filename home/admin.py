09cd6dcb5436370a3958a9ad0119b96283797e72
ryan <kim1885@purdue.edu>
2020-05-12 10:26:12 -0400

    setup admin page

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User, Item, Invoice, LineItem
+
+class InvoiceInline(admin.TabularInline):
+    model = Invoice
+    extra = 3
+
+# class LineItemInline(admin.TabularInline):
+#     model = LineItem
+#     extra = 3
+
+class UserAdmin(admin.ModelAdmin):
+    fieldsets = [('Username', {'fields': ['name']}), ('Phone', {'fields': ['phone_number']}),]
+    inlines = [InvoiceInline]
+
+admin.site.register(User, UserAdmin)
