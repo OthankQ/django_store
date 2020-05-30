a2f3e873ca24d989f851bd6fbfedb8bb3ab325c6
Ubuntu <ubuntu@ip-172-31-5-106.us-east-2.compute.internal>
2020-05-30 22:54:07 +0000

    Switched to https

--- store/settings.py
+++ store/settings.py
@@ -138,3 +138,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
