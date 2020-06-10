01d02240ddfd8cd9ae91ed143d705273fb4d373b
ryan <kim1885@purdue.edu>
2020-06-10 14:16:41 -0400

    Add ItemImage table

--- shop/models.py
+++ shop/models.py
@@ -106,3 +106,11 @@ class Message(models.Model):
 
     def __str__(self):
         return self.message_body
+
+
+class ItemImage(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return str(self.item)
